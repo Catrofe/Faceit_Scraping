676c4e064ff34d4633464d3e60a53d1ecc2073b9
marciorasf <marciorasf@gmail.com>
2022-01-14 20:36:00 -0300

    Melhora os nomes.

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ response = urlopen(f"https://faceitstats.com/player/{nickname}")
 html = response.read().decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
 name = soup.find("h1").getText().strip()
-dados = soup.findAll("div", {"class": "col-6 col-sm-6 col-md-4 col-lg-2"})
+stats_divs = soup.findAll("div", {"class": "col-6 col-sm-6 col-md-4 col-lg-2"})
 
-lista = []
-for item in dados:
-    lista.append(item.find("h5").getText())
+stats = []
+for div in stats_divs:
+    stats.append(div.find("h5").getText())
 
-elo = int(lista[0])
-all_matches = lista[1].split("/")
+elo = int(stats[0])
+all_matches = stats[1].split("/")
 matches = int(all_matches[0].strip())
 won = int(all_matches[1].strip())
 lost = int(all_matches[2].strip())
-win_rate = int(lista[2].replace("%", ""))
-kd = float(lista[3])
-hs_rate = int(lista[4].replace("%", ""))
-win_streak = int(lista[5])
+win_rate = int(stats[2].replace("%", ""))
+kd = float(stats[3])
+hs_rate = int(stats[4].replace("%", ""))
+win_streak = int(stats[5])
 
 InserirNoBanco(name, elo, matches, won, lost, win_rate, kd, hs_rate, win_streak)
