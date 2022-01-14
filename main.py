880b20ddada2c1d4e7610d1700d51d806e75f41e
marciorasf <marciorasf@gmail.com>
2022-01-14 20:21:09 -0300

    Formatando código para o black ficar feliz.

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from urllib.request import urlopen, urlretrieve
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import pandas as pd
 from update import InserirNoBanco, LerBanco
 
-#Recebendo inputs
+# Recebendo inputs
 
 print("Qual seu nick na faceit CS:GO")
 nickname = input(str())
 
 ## Obtendo o HTML e o total de páginas
-response = urlopen('https://faceitstats.com/player/' + nickname)
-html = response.read().decode('utf-8')
-soup = BeautifulSoup(html, 'html.parser')
-name = soup.find('h1').getText().strip()
-dados = soup.findAll('div', {'class': 'col-6 col-sm-6 col-md-4 col-lg-2'})
+response = urlopen("https://faceitstats.com/player/" + nickname)
+html = response.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
+name = soup.find("h1").getText().strip()
+dados = soup.findAll("div", {"class": "col-6 col-sm-6 col-md-4 col-lg-2"})
 
-soup.findAll('div', {'class': 'col-6 col-sm-6 col-md-4 col-lg-2'})
+soup.findAll("div", {"class": "col-6 col-sm-6 col-md-4 col-lg-2"})
 lista = []
 for item in dados:
-    lista.append(item.find('h5').getText())
+    lista.append(item.find("h5").getText())
 
 elo = int(lista[0])
 Matches = lista[1]
@@ -30,7 +29,6 @@ Matches = Matches.split("/")
 matches = int(Matches[0].strip())
 won = int(Matches[1].strip())
 lost = int(Matches[2].strip())
-
 
 
 InserirNoBanco(name, elo, matches, won, lost, win_rate, kd, hs_rate, win_streak)
