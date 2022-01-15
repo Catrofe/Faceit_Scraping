bed65aefb5022a0c2709cd9d72f878f73b2b8022
marciorasf <marciorasf@gmail.com>
2022-01-14 21:36:53 -0300

    Remove código que não faz nada.

--- update.py
+++ update.py
@@ -1,63 +1,48 @@
 import mysql.connector
-import datetime
 
 
 def AbreDB():
-  connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="2020abcd",
-    database="faceit"
-  )
-  cursor = connection.cursor()
-  return connection
+    connection = mysql.connector.connect(
+        host="localhost", user="root", password="2020abcd", database="faceit"
+    )
+    return connection
+
 
 def InserirNoBanco(nome, elo, matches, won, lost, winrate, kd, hs_rate, winstreak):
-  connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="2020abcd",
-    database="faceit"
-  )
-  cursor = connection.cursor()
+    connection = mysql.connector.connect(
+        host="localhost", user="root", password="2020abcd", database="faceit"
+    )
+    cursor = connection.cursor()
 
-  sql = f"""INSERT INTO tbfaceituser (NOME, ELO, MATCHES, WON, LOST, WINRATE, KD, HS_RATE, WINSTREAK) 
+    sql = f"""INSERT INTO tbfaceituser (NOME, ELO, MATCHES, WON, LOST, WINRATE, KD, HS_RATE, WINSTREAK) 
   VALUES('{nome}', {elo}, {matches}, {won}, {lost}, {winrate}, {kd}, {hs_rate}, {winstreak});"""
-                                                                                               
-  data = (
-    'Primeiro Usuário',
-    'primeirousuario@teste.com.br',
-    datetime.datetime.today()
-  )
-  cursor.execute(sql)
-  connection.commit()
 
-  userid = cursor.lastrowid
+    cursor.execute(sql)
+    connection.commit()
 
-  cursor.close()
-  connection.close()
+    userid = cursor.lastrowid
 
-  print("Foi cadastrado o novo usuário de ID:", userid)
+    cursor.close()
+    connection.close()
+
+    print("Foi cadastrado o novo usuário de ID:", userid)
+
 
 def LerBanco():
-  connection = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="2020abcd",
-  database="faceit"
-)
+    connection = mysql.connector.connect(
+        host="localhost", user="root", password="2020abcd", database="faceit"
+    )
 
-  cursor = connection.cursor()
+    cursor = connection.cursor()
 
-  sql = "SELECT * FROM tbfaceituser"
+    sql = "SELECT * FROM tbfaceituser"
 
-  cursor.execute(sql)
-  results = cursor.fetchall()
+    cursor.execute(sql)
+    results = cursor.fetchall()
 
-  cursor.close()
-  connection.close()
+    cursor.close()
+    connection.close()
 
-  print("ID, NOME, ELO, MATCHES, WON, LOST, WINRATE, KD, HS_RATE, WINSTREAK")
-  for result in results:
-    print(result)
-
+    print("ID, NOME, ELO, MATCHES, WON, LOST, WINRATE, KD, HS_RATE, WINSTREAK")
+    for result in results:
+        print(result)
